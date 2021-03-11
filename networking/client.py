d70cc6c0289a41083c6f7472c0bdda00ab9f5890
Skewlock <33401329+Skewlock@users.noreply.github.com>
2021-03-11 22:13:39 +0100

    euh... suis fatigué mais en gros j'ai commenté

--- networking/client.py
+++ networking/client.py
@@ -1,16 +1,16 @@
 import socket
 import threading
-# import hashlib
 
 # class ThreadServer(threading.Thread):
 
 ip_server = "127.0.0.1"
 port = 1000
 
-sock =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((ip_server, port))
 
-def menu():
+
+def menu():  # menu dans la console
     print("-------------------------------------")
     print("\t[Menu de connexion]")
     print("[1]: Connexion")
@@ -18,7 +18,7 @@ def menu():
     print("[3]: Quitter")
     print("-------------------------------------")
     choice = int(input("Votre choix: "))
-    
+
     if choice == 1:
         connexion()
     elif choice == 2:
@@ -29,30 +29,36 @@ def menu():
         pass
 
 
-def connexion():
+def connexion():  # fonction de connexion
     login = input("login: ")
     password = input("password: ")
-    paquet = "login:"+login+":"+password
-    sock.send(paquet.encode("utf-8"))
-    menu()
+    paquet = "login:" + login + ":" + password  # crée le paquet avec login et password
+    sock.send(paquet.encode("utf-8"))  # envoie le paquet
+    r = sock.recv(1024)  # attends une réponse
+    if str(r, "utf-8") == "valid_login":  # si le login est bon
+        print("Mot de passe correct !")
+    else:
+        print("Login ou Mot de passe incorrect !")
+    menu()  # recall le menu
 
 
-def register():
+def register():  # créer un compte
     login = input("Quel est votre login: ")
     password = input("Quel est votre password: ")
     confirm = input("Confirmez votre mot de passe: ")
-    
-    if confirm == password:
-        paquet = "register:"+login+":"+password
-        sock.send(paquet.encode("utf-8"))
+
+    if confirm == password:  # si la confirmation du pass est bonne
+        paquet = "register:" + login + ":" + password  # prépare le paquet
+        sock.send(paquet.encode("utf-8"))  # envoie le paquet
         print("Votre compte a été enregistré.")
     else:
         print("Les mots de passe ne matchent pas.")
-    menu()
+    menu()  # rappelle le menu
 
 
-def quit_():
+def quit_():  # fermer le socket et le programme
     sock.close()
     quit()
+
 
 menu()
--- networking/server.py
+++ networking/server.py
@@ -3,72 +3,87 @@ import threading
 import sqlite3
 
 
+# on déclare le thread pour chaque client
 class ClientThread(threading.Thread):
 
     def __init__(self, ip, port, clientsocket):
         threading.Thread.__init__(self)
-        self.ip = ip
-        self.port = port
-        self.clientsocket = clientsocket
+        self.ip = ip # ip du client
+        self.port = port # port de com
+        self.clientsocket = clientsocket # assez explicite là...
         print("[+] Nouveau thread pour %s %s" % (self.ip, self.port,))
 
     def run(self):
-        print("Connexion de %s %s\n" % (self.ip, self.port,))
-        r = self.clientsocket.recv(1024)
-        text = str(r, "utf-8")
-        command = text.split(":")[0]
-        args = text.split(":")[1:]
+        print("Connexion de %s %s" % (self.ip, self.port,))
+        r = self.clientsocket.recv(1024) # réception du paquet du client
+        text = str(r, "utf-8") # conversion en texte
+        command = text.split(":")[0] # récupération de la commande
+        args = text.split(":")[1:] # récupération des arguments
+        print("[!] Command recieved :")
         print("command: "+ command)
         print("args: "+str(args))
+        print("-------------")
+        # dictionnaire des commandes
         comm = {
             "register": self.register,
             "login": self.login
             }
+        # exécute la commande
         fnc = comm.get(command, lambda: "Commande invalide")
         fnc(args)
     
-    def login(self, args):
-        if len(args) == 2:
-            print("login: ", args[0])
-            print("password: ", args[1])
+    def login(self, args): # commande login
+        if len(args) == 2: # si assez d'arguments fournis
             try:
                 db = sqlite3.connect("db.db")
                 cursor = db.cursor()
-                cursor.execute("SELECT password FROM users WHERE login=?",(args[0],))
+                cursor.execute("SELECT password FROM users WHERE login=?",(args[0],)) # récupère le password
                 pass_ = cursor.fetchone()
-                db.commit()
                 db.close()
-                if pass_[0] == args[1]:
-                    print("le mot de passe est le même")
+                if pass_[0] == args[1]: # si les deux passwords correspondent
+                    print("valid login for %s %s" % (self.ip, self.port))
+                    paquet = "valid_login"
+                    self.clientsocket.send(paquet.encode("utf-8")) # on renvoie le paquet au client pour valider le pass
                 else:
-                    print("mot de passe différent")
+                    print("invalid credentials for %s %s" % (self.ip, self.port))
+                    paquet = "invalid_credentials"
+                    self.clientsocket.send(paquet.encode("utf-8")) # on renvoie le paquet d'erreur
             except TypeError:
-                print("Le login n'existe pas")
-        else:
+                print("invalid credentials for %s %s" % (self.ip, self.port))
+                paquet = "invalid_credentials"
+                self.clientsocket.send(paquet.encode("utf-8")) #paquet d'erreur
+        else: # s'il manque un arg, fait rien
             return
     
     def register(self, args):
-        if len(args) == 2:
-            # print("registering "+ self.ip+" "+ self.port)
+        if len(args) == 2: # si assez d'args
             print("login :"+ args[0])
             print("password: "+args[1])
             try:
                 db = sqlite3.connect("db.db")
                 cursor = db.cursor()
+                # ajoute les crédentials à la db
                 cursor.execute("INSERT INTO users(login, password) VALUES(?,?)", (args[0], args[1]))
                 db.commit()
                 db.close()
-            except:
-                print("Une erreur a eu lieu")
+            except: # si erreur
+                print("error for  %s %s" % (self.ip, self.port))
+                paquet = "error"
+                self.clientsocket.send(paquet.encode("utf-8")) # renvoie le paquet d'erreur
+            else:
+                print("added account for %s %s" % (self.ip, self.port))
+                paquet = "account_added"
+                self.clientsocket.send(paquet.encode("utf-8")) # renvoie le paquet de réussite
         else:
             return
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(("127.0.0.1", 1000))
-sock.listen(0)
+sock.listen(0) # accepte les connexions des sockets
 while True:
     print("Listening...")
+    # accept les sockets et fais un thread par client
     (clientsocket, (ip, port)) = sock.accept()
     client = ClientThread(ip, port, clientsocket)
     client.start()
