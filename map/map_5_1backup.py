6273c2f1dbdca621b22685f221dcd39239603248
Skewlock <Skewlock@gmail.com>
2021-05-05 18:34:18 +0200

    pas grand chose

--- map/map_5_1backup.py
+++ map/map_5_1backup.py
@@ -11,8 +11,8 @@ dic_blocs = {1: pyglet.resource.image("image/herbe1.png"), 2: pyglet.resource.im
              7: pyglet.resource.image("image/terre4.png"), 8: pyglet.resource.image("image/fondtest.png"),
              9: pyglet.resource.image("image/arbre.png")}
 
-#theme = pyglet.resource.media('son/ylia.mp3')
-#theme.play()
+# theme = pyglet.resource.media('son/ylia.mp3')
+# theme.play()
 
 fichier_carte = open("carte.csv", "r", encoding="utf-8", errors='ignore')
 carte = [[int(c) for c in ligne.rstrip().split(";")] for ligne in fichier_carte]
@@ -24,7 +24,6 @@ fichier_decors.close()
 
 
 def create_map():
-    
     L = []  # liste stockant les sprites de blocs
     l = 0  # variable pour la pos des blocs utilisé pour les colonnes
     t = 0
@@ -35,53 +34,55 @@ def create_map():
         z = pyglet.graphics.OrderedGroup(0 - t)
         for j in range(len(carte[i])):
             if carte[i][j] != 0:
-                L.append(pyglet.sprite.Sprite(dic_blocs[carte[i][j]], x=-32+ k + l, y=window.height/2-64 -k/2 + l/2 - 1,
+                L.append(pyglet.sprite.Sprite(dic_blocs[carte[i][j]], x=-32 + k + l,
+                                              y=window.height / 2 - 64 - k / 2 + l / 2 - 1,
                                               batch=batch, group=z))
-                L[-1].update(scale = 2)
+                L[-1].update(scale=2)
             k = k + 32
 
     batch.draw()
+
 
 @window.event
 def on_mouse_release(x, y, button, modifiers):
     mouse_ = [x, y, button]
     if mouse_[2] == mouse.LEFT:
         coords = get_coords_grid(mouse_)
-        print(search_path((coords[0]-11, coords[1]+11)))
-        
+        print(search_path((coords[0] - 11, coords[1] + 11)))
 
 
 def get_coords_grid(mouse_):
     # penser à partir de la case de référence (celle du perso)
-    alpha = -math.atan(1/2)
-    beta = math.atan(1/2)
-    y2 = (mouse_[0]*math.sin(beta)-mouse_[1]*math.cos(beta))/(math.cos(alpha)*math.sin(beta)-math.sin(alpha)*math.cos(beta))
+    alpha = -math.atan(1 / 2)
+    beta = math.atan(1 / 2)
+    y2 = (mouse_[0] * math.sin(beta) - mouse_[1] * math.cos(beta)) / (
+                math.cos(alpha) * math.sin(beta) - math.sin(alpha) * math.cos(beta))
     print("-----")
     print(y2)
-    x2 = (mouse_[1]*math.cos(alpha)-mouse_[0]*math.sin(alpha))/(math.cos(alpha)*math.sin(beta)-math.sin(alpha)*math.cos(beta))
+    x2 = (mouse_[1] * math.cos(alpha) - mouse_[0] * math.sin(alpha)) / (
+                math.cos(alpha) * math.sin(beta) - math.sin(alpha) * math.cos(beta))
     print(x2)
-    coords = ((x2//35.77709), (y2//35.77709)) #-(window.height//2)//32+1) 
+    coords = ((x2 // 35.77709), (y2 // 35.77709))  # -(window.height//2)//32+1)
     print(coords)
     return coords
 
 
 def search_path(coords):
     print(coords)
-    current = (0, 0) # point actuel
-    visited = {(0, 0): 0} # changer par les coordonnés du perso
+    visited = {(0, 0): 0}  # changer par les coordonnés du perso
     traiter = [(0, 0)]
     while len(traiter) != 0:
         current = traiter[0]
         del traiter[0]
         print("visited:", visited)
         print("traiter", traiter)
-        bords = get_walkable(current) # les alentour du point
+        bords = get_walkable(current)  # les alentour du point
         for bord in bords:
             print("bord: ", bord)
-            if not(bord in visited.keys()): # si la bordure n'est pas déjà traitée
+            if not (bord in visited.keys()):  # si la bordure n'est pas déjà traitée
                 traiter.append(bord)
-                visited[bord] = visited[current]+1
-                if bord == coords:
+                visited[bord] = visited[current] + 1
+                if bord == (int(coords[0]), int(coords[1])):
                     break
     print(visited[coords])
     return visited
@@ -97,14 +98,14 @@ def valid(point):
 
 def get_walkable(point):
     borders = []
-    if valid((point[0]+1,point[1])):
-            borders.append((point[0]+1, point[1]))
-    if valid((point[0]-1,point[1])):
-            borders.append((point[0]-1, point[1]))
-    if valid((point[0],point[1]+1)):
-            borders.append((point[0], point[1]+1))
-    if valid((point[0],point[1]-1)):
-            borders.append((point[0], point[1]-1))
+    if valid((point[0] + 1, point[1])):
+        borders.append((point[0] + 1, point[1]))
+    if valid((point[0] - 1, point[1])):
+        borders.append((point[0] - 1, point[1]))
+    if valid((point[0], point[1] + 1)):
+        borders.append((point[0], point[1] + 1))
+    if valid((point[0], point[1] - 1)):
+        borders.append((point[0], point[1] - 1))
     return borders
 
 
@@ -121,27 +122,27 @@ def create_decors():
         k = 0
         t = t + 0.01
         z = pyglet.graphics.OrderedGroup(0 - t)
-        for j in range (len(carte_decors[i])):
+        for j in range(len(carte_decors[i])):
             if carte_decors[i][j] != 0:
-                L_decors.append(pyglet.sprite.Sprite(dic_blocs[carte_decors[i][j]], x= -32 + k + l, y=window.height/2-64 - k/2 + l/2 - 1,
-                                                     batch = batch, group = z))
-                L_decors[-1].update(scale = 2)
+                L_decors.append(pyglet.sprite.Sprite(dic_blocs[carte_decors[i][j]], x=-32 + k + l,
+                                                     y=window.height / 2 - 64 - k / 2 + l / 2 - 1,
+                                                     batch=batch, group=z))
+                L_decors[-1].update(scale=2)
             k = k + 32
-        
-    batch.draw()
-    
 
+    batch.draw()
 
 
 @window.event
 def on_draw():
     window.clear()
-    dic_blocs[8].blit(0,0)
+    dic_blocs[8].blit(0, 0)
     create_map()
     create_decors()
+
 
 def run():
     pyglet.app.run()
 
-run()
 
+run()
